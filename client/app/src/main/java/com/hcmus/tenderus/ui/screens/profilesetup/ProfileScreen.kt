8bfac71829844be999faed364f188a9dcc9e6898
thuythanh04 <thanhthuythuyp@gmail.com>
2024-08-04 21:25:26 +0700

    Package explore & main screen

--- client/app/src/main/java/com/hcmus/tenderus/ui/screens/profilesetup/ProfileScreen.kt
+++ client/app/src/main/java/com/hcmus/tenderus/ui/screens/profilesetup/ProfileScreen.kt
@@ -4,7 +4,6 @@ import com.hcmus.tenderus.ui.screens.report.ReportIssueDialog
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -19,7 +18,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
