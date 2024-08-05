38de6392fd5105bb7ffe5d85a332a2288bcccf42
Dang Nguyen Vu <ng.nguynv@gmail.com>
2024-08-05 11:05:40 +0700

    Upd Server README

--- client/app/src/main/java/com/hcmus/tenderus/MainActivity.kt
+++ client/app/src/main/java/com/hcmus/tenderus/MainActivity.kt
@@ -130,7 +130,7 @@ class MainActivity : ComponentActivity() {
         Log.d(TAG, "Init")
 
         //VM init
-//        val matchListVM = MatchListVM()
+        val matchListVM = MatchListVM()
 
 
         enableEdgeToEdge()
@@ -138,8 +138,8 @@ class MainActivity : ComponentActivity() {
             TenderUSTheme {
                 val navController = rememberNavController()
 //                NavHost(navController, startDestination = "messages") {
-                NavHost(navController, startDestination = "splash") {
-//                    composable("messages") { MatchList(matchListVM = matchListVM) }
+                NavHost(navController, startDestination = "messages") {
+                    composable("messages") { MatchList(matchListVM = matchListVM) }
                     composable("splash") { SplashScreen(navController = navController) }
                     composable("onboarding1") { OnboardingScreen1(navController = navController) }
                     composable("signin") { LoginScreen(navController = navController) }
