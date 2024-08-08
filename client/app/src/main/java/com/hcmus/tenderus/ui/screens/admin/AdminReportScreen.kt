07bd5708fad78e13a4b9da36869555c88f3681c7
thiennbao <thiennbao.204@gmail.com>
2024-08-08 16:56:45 +0700

    Add report detail screen UI

--- client/app/src/main/java/com/hcmus/tenderus/ui/screens/admin/AdminReportScreen.kt
+++ client/app/src/main/java/com/hcmus/tenderus/ui/screens/admin/AdminReportScreen.kt
@@ -1,8 +1,6 @@
 package com.hcmus.tenderus.ui.screens.admin
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -10,17 +8,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Logout
-import androidx.compose.material.icons.filled.BarChart
 import androidx.compose.material.icons.filled.ChevronRight
-import androidx.compose.material.icons.filled.People
-import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -31,10 +24,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -42,9 +33,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.hcmus.tenderus.R
 import com.hcmus.tenderus.model.Report
 import com.hcmus.tenderus.model.Status
+import com.hcmus.tenderus.ui.screens.admin.composable.TopBar
 import com.hcmus.tenderus.ui.theme.TenderUSTheme
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -52,67 +43,7 @@ import java.time.format.DateTimeFormatter
 @Composable
 fun AdminReportScreen(reports: List<Report>) {
     Scaffold(
-        topBar = {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .wrapContentHeight()
-                    .padding(top = 8.dp)
-            ) {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(48.dp)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.logo_topbar),
-                        contentDescription = "Logo",
-                        contentScale = ContentScale.Fit,
-                        modifier = Modifier.fillMaxHeight(0.75f)
-                    )
-                }
-                Row(
-                    horizontalArrangement = Arrangement.SpaceAround,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    IconButton(onClick = { }) {
-                        Icon(
-                            Icons.Filled.PieChart,
-                            contentDescription = "Home",
-                            tint = Color.Gray
-                        )
-                    }
-                    IconButton(onClick = { }) {
-                        Icon(
-                            Icons.Filled.BarChart,
-                            contentDescription = "Home",
-                            tint = Color.Gray
-                        )
-                    }
-                    IconButton(onClick = { }) {
-                        Icon(
-                            Icons.Filled.People,
-                            contentDescription = "Home",
-                            tint = Color.Gray
-                        )
-                    }
-                    IconButton(onClick = { }) {
-                        Icon(
-                            Icons.AutoMirrored.Filled.Logout,
-                            contentDescription = "Home",
-                            tint = Color.Gray
-                        )
-                    }
-                }
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(0.5.dp)
-                        .shadow(0.5.dp)
-                )
-            }
-        },
+        topBar = { TopBar() },
         content = { paddingValues ->
             LazyColumn(modifier = Modifier.padding(paddingValues)) {
                 items(reports) { report ->
