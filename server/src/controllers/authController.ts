cb9d7ffa2f8d852f0055ccb982a5c06d773c8226
Dang Nguyen Vu <ng.nguynv@gmail.com>
2024-08-16 10:41:48 +0700

    Upd no username in password reset

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -7,199 +7,193 @@ import { AdmGetAuth } from "../lib/firebase";
 
 
 const authController = {
-  // GET /api/auth/account
-  getAccount: async (req: Request, res: Response) => {
-    try {
-      const { username } = req.body.username;
-      const account = await db.account.findUnique({ where: { username } });
-      res.status(200).json(account);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Something went wrong" });
-    }
-  },
-
-  // POST /api/auth/register
-  register: async (req: Request, res: Response) => {
-    try {
-      const data: Omit<Account, "id"> & { token?: string } = req.body;
-
-      if (!data.username || !data.password || !data.token) {
-        res.status(400).json({ message: "Bad request" });
-        return;
-      }
-
-      const decodedToken = await AdmGetAuth().verifyIdToken(data.token);
-      const takenUsername = await db.account.count({ where: { username: data.username } });
-      await AdmGetAuth().revokeRefreshTokens(decodedToken.uid);
-
-      if (takenUsername) {
-        res.status(409).json({ message: "This username has already taken" });
-      } else {
-        if (data.email) {
-          if (decodedToken.email !== data.email || !decodedToken.email_verified) {
-            res.status(403).json({ message: "No permission" });
-            return;
-          }
-          await AdmGetAuth().updateUser(decodedToken.uid, { emailVerified: false });
-        } else if (data.phone) {
-          if (decodedToken.phone_number !== data.phone) {
-            res.status(403).json({ message: "No permission" });
-            return;
-          }
-        } else {
-          res.status(400).json({ message: "Bad request" });
-          return;
+    // GET /api/auth/account
+    getAccount: async (req: Request, res: Response) => {
+        try {
+            const { username } = req.body.username;
+            const account = await db.account.findUnique({ where: { username } });
+            res.status(200).json(account);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Something went wrong" });
         }
+    },
 
-        data.FirebaseUID = decodedToken.uid
-        data.password = await bcrypt.hash(data.password, 10);
-        data.role = Role.USER;
-        delete data.token
+    // POST /api/auth/register
+    register: async (req: Request, res: Response) => {
+        try {
+            const request: { username: string, password: string, token: string } = req.body;
 
-        const account = await db.account.create({ data });
-        res.status(200).json({ message: "OK" });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Something went wrong" });
-    }
-  },
+            if (!request.username || !request.password || !request.token) {
+                res.status(400).json({ message: "Bad request" });
+                return;
+            }
 
-  // POST /api/auth/login
-  login: async (req: Request, res: Response) => {
-    try {
-      interface LoginRequest {
-        username: string;
-        password: string;
-        FCMRegToken: string;
-      }
-      const data: LoginRequest = {
-        username: req.body.username,
-        password: req.body.password,
-        FCMRegToken: req.body.FCMRegToken,
-      }
-      if (!data.username || !data.password || !data.FCMRegToken) {
-        res.status(400).json({ message: "Bad request" });
-        return;
-      }
+            const decodedToken = await AdmGetAuth().verifyIdToken(request.token);
+            await AdmGetAuth().revokeRefreshTokens(decodedToken.uid);
 
-      const foundAccount = await db.account.findUnique({ where: { username: data.username } });
-      if (foundAccount) {
-        await db.account.update({ where: { username: data.username }, data: { FCMRegToken: data.FCMRegToken } });
-        const isMatched = await bcrypt.compare(data.password, foundAccount.password);
-        if (isMatched) {
-          const token = jwt.sign(foundAccount, process.env.JWT_KEY as string);
+            if (decodedToken.email) {
+                if (!decodedToken.email_verified) {
+                    res.status(403).json({ message: "No permission" });
+                    return;
+                }
+                await AdmGetAuth().updateUser(decodedToken.uid, { emailVerified: false });
+                const data: Omit<Account, "id" | "phone" | "FCMRegToken"> = {
+                    FirebaseUID: decodedToken.uid,
+                    username: request.username,
+                    email: decodedToken.email,
+                    password: await bcrypt.hash(request.password, 10),
+                    role: Role.USER,
+                }
 
-          const firebaseToken = await AdmGetAuth().createCustomToken(foundAccount.FirebaseUID);
+                await db.account.create({ data });
+                res.status(200).json({ message: "OK" });
+            } else if (decodedToken.phone_number) {
+                const data: Omit<Account, "id" | "email" | "FCMRegToken"> = {
+                    FirebaseUID: decodedToken.uid,
+                    username: request.username,
+                    phone: decodedToken.phone_number,
+                    password: await bcrypt.hash(request.password, 10),
+                    role: Role.USER,
+                }
 
-          const firstTime = await db.profile.findUnique({ where: { username: data.username } }) == null;
+                await db.account.create({ data });
+            } else {
+                res.status(403).json({ message: "Invalid token" });
+                return;
+            }
 
-          res.status(200).json({ token, firebaseToken, firstTime });
-        } else {
-          res.status(401).json({ message: "Wrong password" });
+
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Something went wrong" });
         }
-      } else {
-        res.status(404).json({ message: "Username not found" });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Something went wrong" });
-    }
-  },
+    },
 
-  // POST /api/auth/resetPassword
-  resetPassword: async (req: Request, res: Response) => {
-    try {
-      const data: Omit<Account, "id" | "role"> & { token?: string } = req.body;
+    // POST /api/auth/login
+    login: async (req: Request, res: Response) => {
+        try {
+            interface LoginRequest {
+                username: string;
+                password: string;
+                FCMRegToken: string;
+            }
+            const data: LoginRequest = {
+                username: req.body.username,
+                password: req.body.password,
+                FCMRegToken: req.body.FCMRegToken,
+            }
+            if (!data.username || !data.password || !data.FCMRegToken) {
+                res.status(400).json({ message: "Bad request" });
+                return;
+            }
 
-      if (!data.username || !data.password || !data.token) {
-        res.status(400).json({ message: "Bad request" });
-        return;
-      }
+            const foundAccount = await db.account.findUnique({ where: { username: data.username } });
+            if (foundAccount) {
+                await db.account.update({ where: { username: data.username }, data: { FCMRegToken: data.FCMRegToken } });
+                const isMatched = await bcrypt.compare(data.password, foundAccount.password);
+                if (isMatched) {
+                    const token = jwt.sign(foundAccount, process.env.JWT_KEY as string);
 
-      const decodedToken = await AdmGetAuth().verifyIdToken(data.token);
-      const foundAccount = await db.account.findUnique({ where: { username: data.username } });
-      await AdmGetAuth().revokeRefreshTokens(decodedToken.uid);
+                    const firebaseToken = await AdmGetAuth().createCustomToken(foundAccount.FirebaseUID);
 
-      if (foundAccount) {
-        if (data.email) {
-          if (decodedToken.email !== data.email || !decodedToken.email_verified) {
-            res.status(403).json({ message: "No permission" });
-            return;
-          }
-          if (data.email !== foundAccount.email) {
-            res.status(400).json({ message: "No email match with this username" });
-            return;
-          }
-          await AdmGetAuth().updateUser(decodedToken.uid, { emailVerified: false });
-        } else if (data.phone) {
-          if (decodedToken.phone_number !== data.phone) {
-            res.status(403).json({ message: "No permission" });
-            return;
-          }
+                    const firstTime = await db.profile.findUnique({ where: { username: data.username } }) == null;
 
-          if (data.phone !== foundAccount.phone) {
-            res.status(400).json({ message: "No phone number match with this username" });
-            return;
-          }
-        } else {
-          res.status(400).json({ message: "Bad request" });
-          return;
+                    res.status(200).json({ token, firebaseToken, firstTime });
+                } else {
+                    res.status(401).json({ message: "Wrong password" });
+                }
+            } else {
+                res.status(404).json({ message: "Username not found" });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Something went wrong" });
         }
+    },
 
-        const newPassword = await bcrypt.hash(data.password, 10);
-        const account = await db.account.update({
-          where: { username: data.username },
-          data: { password: newPassword },
-        });
-        res.status(200).json({ message: "OK" });
-      } else {
-        res.json({ message: "Username not found" });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Something went wrong" });
-    }
-  },
+    // POST /api/auth/resetPassword
+    resetPassword: async (req: Request, res: Response) => {
+        try {
+            const data: { password: string, token: string } = req.body
 
-  // POST /api/auth/signOut
-  signOut: async (req: Request, res: Response) => {
-    try {
-      const username = req.body.username;
-      await db.account.update({ where: { username: username }, data: { FCMRegToken: null } });
-      res.status(200).json({ message: "OK" });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Something went wrong" });
-    }
-  },
+            if (!data.password || !data.token) {
+                res.status(400).json({ message: "Bad request" });
+                return;
+            }
 
-  // POST /api/auth/changePassword
-  // changePassword: async (req: Request, res: Response) => {
-  //   try {
-  //     const data: { username: string; password: string; newPassword: string } = req.body;
-  //     const foundAccount = await db.account.findUnique({ where: { username: data.username } });
-  //     if (foundAccount) {
-  //       const isMatched = await bcrypt.compare(data.password, foundAccount.password);
-  //       if (isMatched) {
-  //         const newPassword = await bcrypt.hash(data.newPassword, 10);
-  //         const account = await db.account.update({
-  //           where: { username: data.username },
-  //           data: { password: newPassword },
-  //         });
-  //         res.status(200).json(account);
-  //       } else {
-  //         res.status(401).json({ message: "Wrong password" });
-  //       }
-  //     } else {
-  //       res.status(404).json({ message: "Username not found" });
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //     res.status(500).json({ message: "Something went wrong" });
-  //   }
-  // },
+            const decodedToken = await AdmGetAuth().verifyIdToken(data.token);
+            await AdmGetAuth().revokeRefreshTokens(decodedToken.uid);
+
+            if (decodedToken.email) {
+                if (!decodedToken.email_verified) {
+                    res.status(403).json({ message: "No permission" });
+                    return;
+                }
+                await AdmGetAuth().updateUser(decodedToken.uid, { emailVerified: false });
+                const newPassword = await bcrypt.hash(data.password, 10);
+                await db.account.update({
+                    where: { email: decodedToken.email },
+                    data: { password: newPassword },
+                });
+                res.status(200).json({ message: "OK" });
+            } else if (decodedToken.phone_number) {
+                const newPassword = await bcrypt.hash(data.password, 10);
+                await db.account.update({
+                    where: { phone: decodedToken.phone_number },
+                    data: { password: newPassword },
+                });
+                res.status(200).json({ message: "OK" });
+            } else {
+                res.status(403).json({ message: "Invalid token" });
+            }
+
+
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Something went wrong" });
+        }
+    },
+
+    // POST /api/auth/signOut
+    signOut: async (req: Request, res: Response) => {
+        try {
+            const username = req.body.username;
+            await db.account.update({ where: { username: username }, data: { FCMRegToken: null } });
+            res.status(200).json({ message: "OK" });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Something went wrong" });
+        }
+    },
+
+    // POST /api/auth/changePassword
+    // changePassword: async (req: Request, res: Response) => {
+    //   try {
+    //     const data: { username: string; password: string; newPassword: string } = req.body;
+    //     const foundAccount = await db.account.findUnique({ where: { username: data.username } });
+    //     if (foundAccount) {
+    //       const isMatched = await bcrypt.compare(data.password, foundAccount.password);
+    //       if (isMatched) {
+    //         const newPassword = await bcrypt.hash(data.newPassword, 10);
+    //         const account = await db.account.update({
+    //           where: { username: data.username },
+    //           data: { password: newPassword },
+    //         });
+    //         res.status(200).json(account);
+    //       } else {
+    //         res.status(401).json({ message: "Wrong password" });
+    //       }
+    //     } else {
+    //       res.status(404).json({ message: "Username not found" });
+    //     }
+    //   } catch (error) {
+    //     console.log(error);
+    //     res.status(500).json({ message: "Something went wrong" });
+    //   }
+    // },
 };
 
 export default authController;
